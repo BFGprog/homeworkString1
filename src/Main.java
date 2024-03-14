281e817ca7e11cf8981b4375d8711edad125204e
BFGprog <karpov0005@gmail.com>
2024-03-14 11:46:04 +0300

    Tasks done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.nio.file.SimpleFileVisitor;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,8 +10,23 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
+
+        // Task 2
+        System.out.println("Task 2");
+
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUp);
+
+
+        // Task 3
+        System.out.println("Task 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+
+        System.out.println(fullName);
 
     }
 }
