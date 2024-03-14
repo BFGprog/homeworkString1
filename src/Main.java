c2eb94653cd69ea9204dde36b852f4f8e1fcecc8
BFGprog <karpov0005@gmail.com>
2024-03-14 11:29:21 +0300

    Create a project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Task 1
+        System.out.println("Task 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+
+    }
+}
